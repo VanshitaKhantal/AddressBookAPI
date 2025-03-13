d72f75bfa5a2ce1e2bdefa9bc9f59e87c057f83a
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-13 20:37:52 +0530

    [Vanshita] Add: Implement DTO and Model for Address Book

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e82f3868a20eaf1ad10e05ad8f50b2871d87f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb3d7eb9fd286f53e327b4a20c629525d883369")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
