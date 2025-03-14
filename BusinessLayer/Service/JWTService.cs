4a04e482dc9209bc4a69fcb796502611281e7502
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-14 16:08:34 +0530

    [Vanshita] Add: Implement User Authentication

---
+++ BusinessLayer/Service/JWTService.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Service
+{
+    public class JwtService : IJwtService
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtService(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        /// <summary>
+        /// Generates a JWT token for a given user entity.
+        /// </summary>
+        /// <param name="user">The user entity containing the required information.</param>
+        /// <returns>JWT token as a string.</returns>
+        public string GenerateToken(UserEntity user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _jwtOptions.Issuer,
+                _jwtOptions.Audience,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ RepositoryLayer/Migrations/20250314075954_UpdatedMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class UpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "AddressBookEntries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "AddressBookEntries");
+        }
+    }
+}
