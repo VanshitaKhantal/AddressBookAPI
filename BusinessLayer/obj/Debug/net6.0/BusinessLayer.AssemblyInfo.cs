a57732bc31da3732a72246d9ebf7c9ca3226a458
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-16 21:09:49 +0530

    [Vanshita] Add: Changes made in addressbook controller

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e3311a823ca26558f82632781c6fccfb57a1ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e82f3868a20eaf1ad10e05ad8f50b2871d87f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
