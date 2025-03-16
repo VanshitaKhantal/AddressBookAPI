7c996971a95944ddf460c6cc85f771743e92dbde
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-16 21:44:55 +0530

    [Vanshita] Add: Changes made in redis-cache

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d644a8bbd512679d5dd1cadf77a582cb01132b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1980558834f2c096ab69826fc9aaae96b262f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
