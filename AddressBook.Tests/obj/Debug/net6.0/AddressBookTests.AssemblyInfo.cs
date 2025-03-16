5c6623305b2fe0aede492a6636b01fffc98cfd79
Vanshita Khantal <khantalvanshita@gmail.com>
2025-03-16 20:27:52 +0530

    [Vanshita] Add: Changes made in nunit tests

--- AddressBook.Tests/obj/Debug/net6.0/AddressBookTests.AssemblyInfo.cs
+++ AddressBook.Tests/obj/Debug/net6.0/AddressBookTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d16e96ab601fa75d1151e0c1b788614fd31ae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c865c14b40c3530fd3df1566046222c3ccf0d4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,27 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a76fd1148ea8af2968c57e9a6efa1c71f6ef3cc")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94ab56d85c951316254acb390a8bdf650e39f84")]
->>>>>>> feature-password-reset
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d644a8bbd512679d5dd1cadf77a582cb01132b")]
->>>>>>> feature-redis-cache
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1980558834f2c096ab69826fc9aaae96b262f1")]
->>>>>>> feature-rabbitmq-events
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8ef5680547df3d09633508ac12363dc030ce31")]
->>>>>>> feature-swagger-docs
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d16e96ab601fa75d1151e0c1b788614fd31ae4")]
->>>>>>> feature-nunit-tests
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c865c14b40c3530fd3df1566046222c3ccf0d4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
